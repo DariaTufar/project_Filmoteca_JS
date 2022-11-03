9daba3a3c47dca50c37fa2fda5bb014139dbdf8a
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-11-03 23:49:56 +0200

    structure created

---
+++ src/js/constants.js
@@ -0,0 +1,2 @@
+export const  KEY = "e994a62a86f0219af0993364e0b8b3fc";
+
---
+++ src/js/searchMovies.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const KEY = 'e994a62a86f0219af0993364e0b8b3fc';
+
+//   search/company?api_key=<<api_key>>&page=1
+
+export async function getMovieId() {
+  const query = `queen`;
+  const page = 20;
+
+  const urlSearchByName = `${BASE_URL}search/movie?api_key=${KEY}&query=${query}&page=${page}`;
+
+  console.log(urlSearchByName);
+
+  const response = await axios.get(urlSearchByName);
+
+  console.log(response.data.results);
+  console.log(response.data.results[0].original_title);
+  console.log(response.data.results[0].genre_ids);
+  const movieId = response.data.results[0].id;
+
+  console.log(movieId);
+
+  const posterUrl =
+    `https://www.themoviedb.org/t/p/original/` +
+    response.data.results[0].poster_path;
+  console.log(posterUrl);
+  return movieId;
+}
+ 
+getMovieId();
+
+console.log(movieId);
+ 
