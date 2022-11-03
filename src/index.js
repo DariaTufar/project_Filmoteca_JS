49e03aef4abc871b364fc4ef618d78eb201b8bd9
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-11-03 22:21:26 +0200

    partials  html,  js file added according distributed tasks

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// import   src="./js/constants.js
+
+// //   <script type="module" src="./js/constants.js"></script>
+
+//     <script type="module" src="./js/modal_card_film.js"></script>
+//     <script type="module" src="./js/logoBtnMain.js"></script>
+//     <script type="module" src="./js/watchedBtn.js"></script>
+//     <script type="module" src="./js/addToWatched.js"></script>
+//     <script type="module" src="./js/queueBtn.js"></script>
+//     <script type="module" src="./js/addToQueue.js"></script>
+//     <script type="module" src="./js/spinner.js"></script>
+//     <script type="module" src="./js/createMarkup.js"></script>
+//     <script type="module" src="./js/ popularMoviesMainPage.js"></script>
+   
+//     <script type="module" src="./js/searchMovies.js"></script>
