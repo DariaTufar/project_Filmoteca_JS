2705e0fadb1148f5187adc2aac99d563d9ce4803
Valerii Droniak <droniak.valerii@gmail.com>
2022-11-05 13:40:11 +0200

    Signatures

--- src/js/serverRequest.js
+++ src/js/serverRequest.js
@@ -13,7 +13,7 @@ const configDefault = {
     }
 };
 
-export default class ServerRequest {
+export default class ServerRequest { // При ініціалізації можна передавати базову урлу, та обєкт конфігурацій
     BASE_URL;
 
     constructor(url = BASE_URL, config = configDefault) {
@@ -23,35 +23,35 @@ export default class ServerRequest {
     }
 
     get page() {
-        return this.config.params.page;
+        return this.config.params.page; // Повертає значення параметра сторінки, в конфігурації запиту
     }
 
     set page(newPage) {
-        this.config.params.page = newPage;
+        this.config.params.page = newPage; // Змінює параметр сторінки на будьяке число, в конфігурації запиту
     }
 
     incrementPage() {
-        this.config.params.page += 1;
+        this.config.params.page += 1; // Збільшує параметр сторінки на 1, в конфігурації запиту
     }
 
     decrementPage() {
-        this.config.params.page -= 1;
+        this.config.params.page -= 1; // Зменшує параметр сторінки на 1, в конфігурації запиту
     }
 
     reset() {
-        this.config.params.page = 1;
+        this.config.params.page = 1; // Скидає параметр сторінки в конфігурації запиту 
     }
 
     async getMovies(details = TRENDING_LIST) {
-        this.url = this.BASE_URL + details;
-        const response = await axios.get(this.url, this.config);
+        this.url = this.BASE_URL + details; // Тут додає ться базова урла і більш детальний шлях
+        const response = await axios.get(this.url, this.config); // Запит на АРІ
 
-        return response.data;
+        return response.data; // Повертає проміс із даними
     }
 
-    async getGenres(details = GENRES_LIST) {
-        const response = await axios.get(`${this.BASE_URL}${GENRES_LIST}`, this.config);
+    async getGenres() {
+        const response = await axios.get(`${this.BASE_URL}${GENRES_LIST}`, this.config); // Запит на АРІ за жанрами
 
-        return response.data.genres;
+        return response.data.genres; // Повертає проміс із жанрами
     }
 }
