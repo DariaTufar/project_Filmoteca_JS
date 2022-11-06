3612ba6a21496187a352d7edaa3a22942494713b
svitlanapl <sveta_pr@i.ua>
2022-11-07 00:03:45 +0200

    update

--- src/js/searchMovies.js
+++ src/js/searchMovies.js
@@ -1,24 +1,84 @@
+// import Notiflix from "notiflix";
+// import { KEY } from "./constants";
+// import { refs } from "./refs";
+// import ServerRequest from './serverRequest';
+// import { markupMovieCards } from './markupMovieCards';
+// // import { renderMovieCards } from "./createMarkup";
+
+// Notiflix.Notify.init({
+//   // успіх
+//   // success: {
+//   //   background: 'rgba(255, 107, 8, 1)',
+//   // },
+//   // попередження
+//   warning: {
+//     background: 'rgba(255, 107, 8, 1)',
+//   },
+// });
+
+// const MESSAGE_INFO = 'Sorry, there are no movies matching your search query. Please try again.';
+// const MESSAGE_ERROR = 'Please, enter key word for search!';
+// const MESSAGE_SUCCESS = 'Hooray! We found movies.';
+
+// const configDefault = {
+//   params: {
+//     api_key: KEY,
+//     page: 1,
+//     query: null,
+//     language: 'en-US',
+//   },
+// }
+// const searchAPI = new ServerRequest('search/movie', configDefault);
+
+// refs.formSearch.addEventListener('submit', onSearchForm);
+
+// async function onSearchForm(evt) {
+//   evt.preventDefault();
+
+//   const inputText = refs.inputSearch.value.trim();
+
+//   if (inputText.length === 0) {
+//     Notiflix.Notify.failure(MESSAGE_ERROR); // Робимо перевірку на порожній запит пошуку.
+//     return false;
+//   }
+
+//   searchAPI.query = inputText;
+
+//   //await renderPageMovies(searchAPI);
+
+//   const { results } = await searchAPI.getMovies();
+//   const genres = await searchAPI.getGenres(); // Повертає жанри з АРІ
+//   const markup = await markupMovieCards(results, genres);
+
+//   refs.galleryList.innerHTML = markup.join('');
+
+//   try {
+//     if (results.length > 0) {
+//       Notiflix.Notify.success(MESSAGE_SUCCESS);
+//     }
+//     else {
+//       Notiflix.Notify.warning(MESSAGE_INFO);
+//     }
+  
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 import Notiflix from "notiflix";
 import { KEY } from "./constants";
 import { refs } from "./refs";
 import ServerRequest from './serverRequest';
-import { markupMovieCards } from './markupMovieCards';
-// import { renderMovieCards } from "./createMarkup";
+import { renderPageMovies } from "./createMarkup";
 
 Notiflix.Notify.init({
-  // успіх
-  // success: {
-  //   background: 'rgba(255, 107, 8, 1)',
-  // },
-  // попередження
-  warning: {
+  info: {
     background: 'rgba(255, 107, 8, 1)',
   },
 });
 
-const MESSAGE_INFO = 'Sorry, there are no movies matching your search query. Please try again.';
-const MESSAGE_ERROR = 'Please, enter key word for search!';
-const MESSAGE_SUCCESS = 'Hooray! We found movies.';
+const SEARCH_LIST = 'search/movie';
+const MESSAGE_INFO = 'Please, enter key word for search!';
+const MESSAGE_ERROR = 'Sorry, there are no movies matching your search query. Please try again.';
 
 const configDefault = {
   params: {
@@ -28,7 +88,8 @@ const configDefault = {
     language: 'en-US',
   },
 }
-const searchAPI = new ServerRequest('search/movie', configDefault);
+
+const searchAPI = new ServerRequest(SEARCH_LIST, configDefault);
 
 refs.formSearch.addEventListener('submit', onSearchForm);
 
@@ -37,29 +98,24 @@ async function onSearchForm(evt) {
 
   const inputText = refs.inputSearch.value.trim();
 
-  if (inputText.length === 0) {
-    Notiflix.Notify.failure(MESSAGE_ERROR); // Робимо перевірку на порожній запит пошуку.
-    return false;
+  if (!inputText) {
+    Notiflix.Notify.info(MESSAGE_INFO); 
+    return;
   }
 
   searchAPI.query = inputText;
 
-  //await renderPageMovies(searchAPI);
-
-  const { results } = await searchAPI.getMovies();
-  const genres = await searchAPI.getGenres(); // Повертає жанри з АРІ
-  const markup = await markupMovieCards(results, genres);
-
-  refs.galleryList.innerHTML = markup.join('');
-
   try {
-    if (results.length > 0) {
-      Notiflix.Notify.success(MESSAGE_SUCCESS);
+       await renderPageMovies(searchAPI);
+    if (searchAPI.totalMovies) {
+      Notiflix.Notify.success(`Hooray! We found ${searchAPI.totalMovies} movies`);
     }
     else {
-      Notiflix.Notify.warning(MESSAGE_INFO);
+      throw new Error(MESSAGE_ERROR);
     }
+  
   } catch (error) {
-    console.log(error.message);
+    Notiflix.Notify.failure(error.message);
   }
 }
+
