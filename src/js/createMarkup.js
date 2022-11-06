77ecc85de810cab82cf77c1f7ce62cdd436942ea
Valerii Droniak <droniak.valerii@gmail.com>
2022-11-06 14:17:13 +0200

    final

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,11 +1,9 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { KEY } from './constants';
 import { refs } from './refs';
+import { markupMovieCards } from './markupMovieCards';
 import ServerRequest from './serverRequest';
-import posterDefault from '../images/posterMovie.jpg';
 
 const TRENDING_LIST = 'trending/movie/week'; // Уточнюючий шлях для запиту
-const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500'; // Базова урла для зображень, та постерів
 
 export const movieAPI = new ServerRequest(TRENDING_LIST); // Ініціалізує екземпляр класу для запитів на АРІ. При ініціалізації потрібно передати детальний шлях який додається до базової урли АРІ (БАЗОВА УРЛА ВЖЕ ПРИСУТНЯ В КЛАСІ!!!!!!), та обєкт конфігурацій
 
@@ -18,35 +16,11 @@ export async function renderPageMovies(requestAPI = movieAPI) { // Робить 
     requestAPI.totalPages = total_pages;
     requestAPI.totalMovies = total_results;
 
-    const markup = await renderMovieCards(results); // Рендерить розмітку для карток
+    const genres = await requestAPI.getGenres(); // Повертає жанри з АРІ
+    const markup = await markupMovieCards(results, genres); // Рендерить розмітку для карток
 
     refs.galleryList.innerHTML = markup.join(''); // Додає розмітку в DOM
   } catch (error) {
     Notify.failure(error.message);
   }
-}
-
-export async function renderMovieCards(movies) {
-  const genres = await movieAPI.getGenres(); // Повертає жанри з АРІ
-  // Масив розмітки всіх карток фільмів що прийшли з АРІ
-  return movies.map(({ title, poster_path, release_date, genre_ids, id }) => {
-    const poster = poster_path ? BASE_URL_IMG + poster_path : posterDefault; // Генерує потрібний постер, в залежності від того чи він прийшов з АРІ
-    const releaseYear = release_date.split('').slice(0, 4).join(''); // Парсить дату релізу та повертає тільки рік
-    // Повертає жанри для конкретного фільму
-    const genresName = genres
-      .filter(genre => genre_ids.includes(genre.id))
-      .map(el => el.name);
-
-    if (genresName.length > 2) {  // Перевіряє довжину масиву жанрів чи вона більше 2-х
-      genresName.splice(2, genresName.length, 'Other'); // Обрізає масив жанрів та додає в кінець "Other"
-    }
-
-    return `<li class="gallery__item" data-movieID="${id}">
-              <img src="${poster}" alt="${title}" class="gallery__item-poster">
-               <div class=" gallery__item-descr-wrap">
-                 <h2 class="gallery__item-title">${title}</h2>
-                 <p class="gallery__item-genre">${genresName.join(', ')} <span class="gallery__item-date">| ${releaseYear}</span></p>
-              </div>
-            </li>`;
-  });
 }
