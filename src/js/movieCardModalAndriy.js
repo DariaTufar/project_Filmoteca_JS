ae14c0bf57b100e0cc4fd18d7cbc052236575d34
Andry1343 <ua.gavrilets@gmail.com>
2022-11-08 16:58:13 +0200

    Update_modal
    Добавил необходимые кнопки и с тили к ним.

--- src/js/movieCardModalAndriy.js
+++ src/js/movieCardModalAndriy.js
@@ -1,14 +1,27 @@
 import { KEY } from './constants';
+//import { refs } from './refs';
 import axios from 'axios';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
+
+/* const refs = {
+  galleryOnClick: document.querySelector('#data-movieid'),
+  formSearch: document.querySelector('#js-search-form')
+}
+
+console.log(refs.formSearch)
+console.log(refs.galleryOnClick)
+
+refs.galleryOnClick.addEventListener('click', IdMovie);
+const IdMovie = refs.galleryOnClick
+console.log(IdMovie) */
 
 
 async function findMovieByID() {
  const params = {
     baseUrl: 'https://api.themoviedb.org/3/',
     fotoUrl: 'https://image.tmdb.org/t/p/w500',
-    movie_id: 88,
+    movie_id: 89,
     language: 'en-US',
     api_key: KEY,
   };
@@ -23,7 +36,7 @@ async function findMovieByID() {
     const markup = 
       `<div class="modal_description">
       <div class="movie_div">
-          <img class="foto" src="${params.fotoUrl}${poster_path}" alt="poster_foto ">
+          <img class="movie_foto" src="${params.fotoUrl}${poster_path}" alt="poster_foto ">
       </div>
       <div class="film_information">
           <h1 class="movie_title">${title}</h1>
@@ -37,11 +50,29 @@ async function findMovieByID() {
           <h2 class="movie_about">ABOUT</h2>
           <p class="about_text">${overview}</p>
 
-          <div class="movie_btn">
-          <button type="radio" name="add_watched_btn" class="film_btn">ADD TO WATCHED</button>
-          <button type="radio" name="add_to_queue_btn" class="film_btn">ADD TO QUEUE</button>
-          <button type="radio" name="trailer_btn" class="film_btn">TRAILER</button>
-      </div>
+          <form class="js-movie-buttons movie-buttons">
+  <label class="movie-buttons__label">
+    <input
+      class="movie-buttons__input"
+      type="radio"
+      name="status"
+      value="isWatched"
+    />
+    <span class="movie-buttons__text button">Watched</span>
+  </label>
+
+  <label class="movie-buttons__label">
+    <input
+      class="movie-buttons__input"
+      type="radio"
+      name="status"
+      value="isQueued"
+    />
+    <span class="movie-buttons__text button">Queued</span>
+  </label>
+  <button type="button" class="js-remove-button button">Remove</button>
+  <button type="button" name="trailer_btn" class="button trailer_btn">TRAILER</button>
+</form>
       </div>
     </div>`
 
@@ -64,3 +95,9 @@ async function findMovieByID() {
 
 findMovieByID()
 export { findMovieByID };
+
+/* <div class="movie_btn">
+          <button type="radio" name="add_watched_btn" class="film_btn">ADD TO WATCHED</button>
+          <button type="radio" name="add_to_queue_btn" class="film_btn">ADD TO QUEUE</button>
+          <button type="radio" name="trailer_btn" class="film_btn">TRAILER</button>
+      </div> */
