91d14e1cf6435886d4b0add42552f312aa783cbb
Nickolya0604 <nickolya.od@gmail.com>
2022-11-09 19:44:32 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,31 +1,21 @@
 import * as basicLightbox from 'basiclightbox';
+import { KEY } from './constants';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
-const key = 'e994a62a86f0219af0993364e0b8b3fc';
+const srcTrailer = 'https://www.youtube.com/embed/';
 
-function findTrailer(idCard) {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${idCard}/videos?api_key=${key}&language=en-US`
-  ).then(respons => respons.json());
-}
-
-const movieTrailer = () => {
-  const trailerBtn = document.querySelector('.trailer_btn');
-
-  trailerBtn.addEventListener('click', function (e) {
-    openTrailer(e.target.dataset.id);
-  });
-};
-
-function openTrailer(id) {
-  findTrailer(id)
-    .then(data => {
-      console.log(data);
-      const key = data.results[0].key;
-      const modal = basicLightbox.create(`
-  <iframe width="680" height="415" src="https://www.youtube.com/embed/${key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+const modal = basicLightbox.create(`
+  <iframe width="680" height="415" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <button class="close-modal__trailer">     
      </button>`);
+const iframeTrailer = modal.element().querySelector('iframe');
+
+export default function openTrailer(id) {
+  findTrailer(id)
+    .then(data => {
+      const key = data.results[0].key;
+
+      iframeTrailer.src = srcTrailer + key;
       modal.show();
 
       const closeBtn = document.querySelector('.close-modal__trailer');
@@ -37,10 +27,15 @@ function openTrailer(id) {
     })
     .catch(error => {
       const modal = basicLightbox.create(`
-    <iframe width="860" height="615" src="https://www.youtube.com/embed/6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="860" height="615" src="${srcTrailer}6DhiiFGk4_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       `);
       modal.show();
+      console.log(error);
     });
 }
 
-export default movieTrailer;
+function findTrailer(idCard) {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${idCard}/videos?api_key=${KEY}&language=en-US`
+  ).then(respons => respons.json());
+}
