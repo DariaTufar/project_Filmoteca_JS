30faa7700f621a9dd5c978d212f76045129e1e2f
Ekaterina Silchenko <maksil.17@gmail.com>
2022-11-10 23:06:30 +0200

    добавляет изменения

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -2,13 +2,14 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { refs } from './refs';
 import { markupMovieCards } from './markupMovieCards';
 import ServerRequest from './serverRequest';
-import { showSpinner, hideSpinner } from "./spinner";
+import { showSpinner, hideSpinner } from './spinner';
 
 // import createPagination from './createPagination';
 
 const TRENDING_LIST = 'trending/movie/week'; // Уточнюючий шлях для запиту
 const MESSAGE_INFO = 'Please, enter key word for search!';
-const MESSAGE_ERROR = 'Sorry, there are no movies matching your search query. Please try again.';
+const MESSAGE_ERROR =
+  'Sorry, there are no movies matching your search query. Please try again.';
 
 const movieAPI = new ServerRequest(TRENDING_LIST);
 
@@ -42,8 +43,11 @@ export async function dataMovies(requestAPI) {
   }
 }
 
-function onClickPagination(event) {
-  if (event.target === event.currentTarget || event.target.textContent === '...') {
+export function onClickPagination(event) {
+  if (
+    event.target === event.currentTarget ||
+    event.target.textContent === '...'
+  ) {
     return;
   }
 
@@ -91,7 +95,6 @@ async function onSearchForm(evt) {
   hideSpinner(refs.spinner, refs.iconSearch);
 }
 
-
 //======================================================================================================//
 // export async function renderPageMovies(requestAPI) { // Робить запит та рендерить розмітку даних
 
@@ -112,9 +115,7 @@ async function onSearchForm(evt) {
 //     console.log(requestAPI.page);
 //     console.log('query >>', requestAPI.query);
 
-
 //     requestAPI.createPagination(refs.element);
-
 
 //     if (requestAPI.page === 1) {
 //       addListner(requestAPI);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,14 +1,14 @@
 export const refs = {
-    formSearch: document.querySelector('#js-search-form'),
-    inputSearch: document.querySelector('.search__form-input'),
-    galleryList: document.querySelector('.gallery_main-page'),
-    addWatched: document.querySelector('.add-watched'),
-    addQueue: document.querySelector('.add-queue'),
-
-    element: document.querySelector(".pagination ul"),
-
-    buttonSearch: document.querySelector('.search__button'),
-    spinner: document.querySelector('.spinner'),
-    iconSearch: document.querySelector('.icon-search'),
- 
-}
+  formSearch: document.querySelector('#js-search-form'),
+  inputSearch: document.querySelector('.search__form-input'),
+  galleryList: document.querySelector('.gallery_main-page'),
+  addWatched: document.querySelector('.add-watched'),
+  addQueue: document.querySelector('.add-queue'),
+  element: document.querySelector('.pagination ul'),
+  button: document.querySelector('.dropbtn'),
+  dropdownContent: document.querySelector('.dropdown-content'),
+  dropdown: document.querySelector('.dropdown'),
+  buttonSearch: document.querySelector('.search__button'),
+  spinner: document.querySelector('.spinner'),
+  iconSearch: document.querySelector('.icon-search'),
+};
